abf65c65fe6f5b5d4829d8a73fd3be3174f5d8eb
Ömer Çalışkan <50463226+omerclskn@users.noreply.github.com>
2019-05-12 23:07:18 +0300

    Update NewtonRaphson_Method.c

--- NewtonRaphson_Method.c
+++ NewtonRaphson_Method.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 float FunctionResult(float equation[], int N, float x)
 {
@@ -27,7 +28,7 @@ float DerivateResult(float equation[], int N, float x){
 
 int main() {
 	float equation[50], x0, fx0, epsilon, fdx0, x1,control=0.0;
-    int i, degree;
+    int i, degree, step=1;
 
     printf("Degree of Equation :");
     scanf("%d", &degree);
@@ -44,14 +45,18 @@ int main() {
     printf("Epsilon : ");
     scanf("%f", &epsilon);
 	
+	printf("Step\tx0\tx1\n");
+	
 	do{	
 	fx0 = FunctionResult(equation, degree, x0);
 	fdx0 = DerivateResult(equation, degree, x0);
 	x1 = x0 - (fx0/fdx0);
 	control = x1-x0;
+	printf("%d\t%0.3f\t%0.3f\n",step, x0, x1);
 	x0 = x1;
-	if(control < 0) control *= -1;
-	}while(epsilon < control);
+	
+	step++;
+	}while(epsilon < fabs(control));
 
 	printf("Root: %f\n",x0);
     system("pause");
