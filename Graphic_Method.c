86ae98989e784dd79a5fe435cf5bc2a8aa1cf88f
Ömer Çalışkan <50463226+omerclskn@users.noreply.github.com>
2019-05-12 22:25:55 +0300

    Update Graphic_Method.c

--- Graphic_Method.c
+++ Graphic_Method.c
@@ -16,7 +16,7 @@ float FunctionResult(float equation[], int N, float x)
 int main()
 {
     float equation[50], x0, x1, f0, f1, dx, epsilon;
-    int i, degree;
+    int i, degree, step=1;
 
     printf("Degree of Equation :");
     scanf("%d", &degree);
@@ -35,21 +35,24 @@ int main()
 
     printf("Epsilon : ");
     scanf("%f", &epsilon);
-
-
+	
+	printf("\nStep\tx0\t\tf(x0)\t\tx1\t\tf(x1)\n");	
+	
     while(epsilon < dx)
     {
     f0 = FunctionResult(equation, degree, x0);
 
     x1 = x0 + dx;
     f1 = FunctionResult(equation, degree, x1);
-
+	printf("%d\t%f\t%f\t%f\t%f\n", step, x0, f0, x1, f1);
+	
     if(f0 * f1 < 0)
         dx /= 2.0;
     else
         x0 = x1;
+        step++;
     }
-    printf("Root: %f\n", x0);
+    printf("\nRoot: %f\n", x0);
     system("pause");
     return 0;
 }
